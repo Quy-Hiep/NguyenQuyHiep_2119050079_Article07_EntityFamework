c84a9bde616397341be980d96e23314db878d109
Quý Hiệp Nguyễn <nquyhiep@gmail.com>
2022-01-05 08:14:10 +0700

    add Area_BUS

---
+++ BUS/Area_BUS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+namespace BUS
+{
+    public class Area_BUS
+    {
+        Area_DAL dal = new Area_DAL();
+        public List<Area_DTO> ReadAreaList()
+        {
+            List<Area_DTO> lstArea = dal.ReadArea();
+            return lstArea;
+        }
+        public void NewArea(Area_DTO area)
+        {
+            dal.NewArea(area);
+        }
+        public void DeleteArea(Area_DTO area)
+        {
+            dal.DeleteArea(area);
+        }
+        public void EditArea(Area_DTO area)
+        {
+            dal.EditArea(area);
+        }
+    }
+}
