296cca97768656a80777b6bab9ba99fe53561a4a
Quý Hiệp Nguyễn <nquyhiep@gmail.com>
2022-01-05 08:04:15 +0700

    create AreaDAL

---
+++ DAL/AreaDAL.cs
@@ -0,0 +1,33 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class AreaDAL:DBConnection
+    {
+        public List<Area_DTO> ReadArea()
+        {
+            return Areas.ToList();
+        }
+        public void DeleteArea(Area_DTO area)
+        {
+            this.Areas.Remove(area);
+            this.SaveChanges();
+        }
+        public void NewArea(Area_DTO area)
+        {
+            this.Areas.Add(area);
+            this.SaveChanges();
+        }
+        public void EditArea(Area_DTO area)
+        {
+            this.Entry(area).State = EntityState.Modified;
+            this.SaveChanges();
+        }
+    }
+}
